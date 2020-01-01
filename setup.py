1599b00e275ff2e4f5aaa58d874e1f671804b8ee
Konrad Weihmann <kweihmann@outlook.com>
2020-01-01 13:32:03 +0100

    Include requirements.txt to source release
    Update release to 1.0.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="ensurecvs",
-    version="1.0.1",
+    version="1.0.2",
     author="Konrad Weihmann",
     author_email="kweihmann@outlook.com",
     description="Find important upstream fixes",
